504d59e89f33231711f16cf2c4d112a7937e2bf0
Anahita Doosti <anna.doosti.1995@gmail.com>
2023-05-07 20:40:48 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Anahita Doosti on 2023-05-07.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
